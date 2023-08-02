89f61f5a1d22615eb008aa2b66b75f0a524f5aca
Thomas Couderc <thomas.couderc@gmail.com>
2023-08-02 17:37:53 +0200

    core(): add wordpress code integration

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
+
 /**
  * Plugin Name: Media Info Tracker
  * Description: Ce plugin récupère les url des médias pour chaque page et crée un tableau avec la liste des médias, leur url et un indicateur pour savoir s'ils ont été validés.
  * Version: 1.0
  * Author: Thomas Couderc
  */
-
