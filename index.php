0ca4ce9c41fd11750cf62ff9e84a71f8be191a5f
Thomas Couderc <thomas.couderc@gmail.com>
2023-08-02 17:38:42 +0200

    feat(activation) plugin activation check

--- index.php
+++ index.php
@@ -6,3 +6,19 @@
  * Version: 1.0
  * Author: Thomas Couderc
  */
+
+
+// Elementor plugin installation check 
+
+register_activation_hook(__FILE__, 'media_info_tracker_activation');
+
+function media_info_tracker_activation()
+{
+    if (!is_plugin_active('elementor/elementor.php')) {
+        // Désactivez votre plugin.
+        deactivate_plugins(plugin_basename(__FILE__));
+
+        // Créez un message d'erreur pour informer l'utilisateur.
+        wp_die(__('Ce plugin nécessite Elementor pour fonctionner. Veuillez installer et activer Elementor, puis réessayez.', 'media-info-tracker'));
+    }
+}
